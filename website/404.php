8efdfa2d2d0235436b1f77dabe7fa13426e8340e
robertmeans <robert@robertmeans.com>
2017-10-31 15:15:23 -0600

    new htaccess & imported font

--- website/404.php
+++ website/404.php
@@ -16,17 +16,12 @@
 	<meta http-equiv="pragma" content="no-cache">
 	<meta http-equiv="expires" content="0">
 	
-	<title>Evergreen Brewery, Evergreen, Colorado Brewery &amp; Taphouse</title>
+	<title>404 - Page not found</title>
 	<link rel="icon" type="image/ico" href="_images/favicon.ico">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	<meta name="description" content="Evergreen Brewery in Evergreen, Colorado - Local hand crafted beer">
 	<meta name="format-detection" content="telephone=no">
-	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" type="text/css">
 	<link href='https://fonts.googleapis.com/css?family=Muli:400,400i|Lora:400,400i' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="style.css?<?php echo time(); ?>" type="text/css">
-	<script src="https://code.jquery.com/jquery-latest.js"></script>
-	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCcS8gagJkuoY6gB3utE1iyZnWk4JG5Suw&callback=initMap"></script>
-	<script src="js/google-map.js"></script>
 
 	<!-- Global site tag (gtag.js) - Google Analytics -->
 	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-107373941-1"></script>
@@ -38,3 +33,12 @@
 	  gtag('config', 'UA-107373941-1');
 	</script>
 </head>
+<body>
+	<div id="not-found">
+		<p class="pg-not-found">You seem lost.<br />
+			<a href="/">Click here</a> and we'll get you home safe.</p>
+	</div>
+
+	<script src="http://localhost:35729/livereload.js"></script>
+</body>
+</html>
