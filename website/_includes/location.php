f2b1b1e9e8b0cfcfabb9b2b5891e89398da2ad78
Robert Means <robert@robertmeans.com>
2018-08-04 11:50:44 -0600

    updated hours

--- website/_includes/location.php
+++ website/_includes/location.php
@@ -7,7 +7,8 @@
 			<p><span class="sm-gone"><i class="fa fa-phone-square"></i></span><a class="loc" tabIndex="-1" href="tel:(303)%20736-8419"><span itemprop="telephone">(303) 736-8419</span></a></p>
 		</div><!-- .location -->		
 		<div class="hours location-content">
-			<p><time itemprop="openingHours" datetime="Mo,Tu,We,Th 13:00-22:00">Monday - Thursday 12PM-10PM</time></p>
+			<p><time itemprop="openingHours" datetime="Mo,Tu 15:00-22:00">Monday &amp; Tuesday 3PM-10PM</time></p>
+			<p><time itemprop="openingHours" datetime="We,Th 12:00-22:00">Wednesday &amp; Thursday 12PM-10PM</time></p>
 			<p><time itemprop="openingHours" datetime="Fr,Sa 11:00-23:00">Friday &amp; Saturday 11AM-11PM</time></p>
 			<p><time itemprop="openingHours" datetime="Su 11:00-21:00">Sunday 11AM-9PM</time></p>
 		</div><!-- #hours -->
