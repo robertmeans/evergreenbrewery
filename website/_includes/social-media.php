40ab1615b384dce0711a4ab513a3369b6e7db9de
Robert Means <robert@robertmeans.com>
2019-03-13 19:38:53 -0600

    laptop snowday updates

--- website/_includes/social-media.php
+++ website/_includes/social-media.php
@@ -1,24 +1,26 @@
 <div id="social">
-	<h1 itemprop="name">Social Corner</h1>
-	<h2>At a Glance</h2>
-	<p class="social-intro">Find events. Get notifications</p>
 		<div id="social-container">
+			<h1 itemprop="name">Social Corner</h1>
+			<h2>At a Glance</h2>
+			<p class="social-intro">Find events. Get notifications</p>
 
-			<div id="facebook-block">
-				<img class="social-img" src="_images/facebook-transparent.png">
-				<p>Like us on Facebook</p>
-				<div id="facebook-container">
-					<div class="fb-page" data-href="https://www.facebook.com/evergreenbrewery/" data-tabs="timeline, events" data-width="500px" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/evergreenbreweryandtaphouse/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/evergreenbreweryandtaphouse/">Evergreen Brewery &amp; Tap House</a></blockquote></div>
-				</div><!-- #facebook-container -->
-			</div><!-- #facebook-block -->
-		
-			<div id="instagram-block">
-				<img class="social-img" src="_images/instagram-transparent.png">
-				<p>Follow us on Instagram</p>
-				<div id="instagram-container">
-					<div id="pixlee_container"></div><script type="text/javascript">window.PixleeAsyncInit = function() {Pixlee.init({apiKey:'c8MzrC0zfPY8YEttRmHf'});Pixlee.addSimpleWidget({widgetId:'16438'});};</script><script src="//instafeed.assets.pixlee.com/assets/pixlee_widget_1_0_0.js"></script>
-				</div><!-- #instagram-container --> 
-			</div><!-- #instagram-block -->
+			<div id="social-format">
+				<div id="facebook-block">
+					<img class="social-img" src="_images/facebook-transparent.png">
+					<p>Like us on Facebook</p>
+					<div id="facebook-container">
+						<div class="fb-page" data-href="https://www.facebook.com/evergreenbrewery/" data-tabs="timeline, events" data-width="500px" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/evergreenbreweryandtaphouse/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/evergreenbreweryandtaphouse/">Evergreen Brewery &amp; Tap House</a></blockquote></div>
+					</div><!-- #facebook-container -->
+				</div><!-- #facebook-block -->
+			
+				<div id="instagram-block">
+					<img class="social-img" src="_images/instagram-transparent.png">
+					<p>Follow us on Instagram</p>
+					<div id="instagram-container">
+						<div id="pixlee_container"></div><script type="text/javascript">window.PixleeAsyncInit = function() {Pixlee.init({apiKey:'c8MzrC0zfPY8YEttRmHf'});Pixlee.addSimpleWidget({widgetId:'16438'});};</script><script src="//instafeed.assets.pixlee.com/assets/pixlee_widget_1_0_0.js"></script>
+					</div><!-- #instagram-container --> 
+				</div><!-- #instagram-block -->
+			</div><!-- #social-format -->
 
 		</div><!-- #social-container -->
 </div><!-- #social -->
