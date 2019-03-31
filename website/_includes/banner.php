993b4b1b2c7c5a8bb39b1783f60a9acc32b9110d
Robert Means <robert@robertmeans.com>
2019-03-31 06:42:33 -0600

    social icons and map
    swapped out the social icons on banner for black and white ones and replaced the map with an image file instead that links directly to the apple.map url in order to process for appropriate operating system.

--- website/_includes/banner.php
+++ website/_includes/banner.php
@@ -1,9 +1,9 @@
 <section id="banner">
-	<span onclick="openNav()"><img id="banner-nav-toggle" class="nav-toggle" src="_images/menu-nav-toggle.png" /></span>
+	<span onclick="openNav()"><img id="banner-nav-toggle" class="nav-toggle" src="_images/menu-nav-toggle.png" alt="Navigation Toggle"></span>
 
 	<div id="info-bug">
 		<div class="top-nowrap">
-			<a class="loc" tabIndex="-1" href="tel:(303)%20736-8419"><img class="img-one" src="_images/phone.png"></a><a class="loc" tabIndex="-1" href="http://maps.apple.com/?q=39.660609,-105.352111" target="_blank"><img class="img-two" src="_images/map.png"></a>
+			<a class="loc" tabIndex="-1" href="tel:(303)%20736-8419"><img class="img-one" src="_images/phone.png" alt="(303) 736-8419"></a><a class="loc" tabIndex="-1" href="http://maps.apple.com/?q=39.660609,-105.352111" target="_blank"><img class="img-two" src="_images/map.png" alt="Get Directions"></a>
 		</div>
 		<div id="hours-wrapper">
 			<span id="hours-background"></span>
@@ -12,8 +12,8 @@
 	</div><!-- #info-bug -->
 
 	<div id="social-homepage">
-		<a href="https://www.facebook.com/evergreenbrewery/" target="_blank" onclick="closeNav()"><img src="_images/facebook.png"></a>
-		<a href="https://www.instagram.com/evergreenbrewery/" target="_blank" onclick="closeNav()"><img src="_images/instagram.png"></a>
+		<a href="https://www.facebook.com/evergreenbrewery/" target="_blank" onclick="closeNav()"><img src="_images/facebook-bw.png" alt="Facebook"></a>
+		<a href="https://www.instagram.com/evergreenbrewery/" target="_blank" onclick="closeNav()"><img src="_images/instagram-bw.png" alt="Instagram"></a>
 	</div>
 	<!-- 
 	<img itemprop="logo" id="logo" class="logo" src="_images/evergreen-brewery-bw-logo.png" /> 
