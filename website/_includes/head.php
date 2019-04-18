090a8a1786882587d47838129ca425f30f0e2af3
Evergreen Web Design <info@evergreenwebdesign.com>
2019-04-18 10:55:57 -0600

    updated meta description

--- website/_includes/head.php
+++ website/_includes/head.php
@@ -19,7 +19,7 @@
 	<title>Evergreen Brewery, Evergreen, Colorado Brewery &amp; Taphouse</title>
 	<link rel="icon" type="image/ico" href="_images/favicon.ico">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	<meta name="description" content="Evergreen Brewery in Evergreen, Colorado - Local hand crafted beer">
+	<meta name="description" content="Evergreen Brewery in Evergreen, Colorado - Local hand crafted beer & artisan cuisine.">
 	<meta name="format-detection" content="telephone=no">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" type="text/css">
 	<link href='https://fonts.googleapis.com/css?family=Muli:400,400i|Lora:400,400i' rel='stylesheet' type='text/css'>
