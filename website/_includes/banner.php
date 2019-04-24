0c6140248c25a96ab08a8188dab1104114731a98
Evergreen Web Design <info@evergreenwebdesign.com>
2019-04-24 12:09:39 -0600

    added Order Online to Info Bug

--- website/_includes/banner.php
+++ website/_includes/banner.php
@@ -2,20 +2,16 @@
 	<span onclick="openNav()"><img id="banner-nav-toggle" class="nav-toggle" src="_images/menu-nav-toggle.png" alt="Navigation Toggle"></span>
 
 	<div id="info-bug">
-		<div class="top-nowrap">
+		<div class="info-bug-nowrap">
 			<a class="loc" tabIndex="-1" href="tel:(303)%20736-8419"><img class="img-one" src="_images/phone.png" alt="(303) 736-8419"></a><a class="loc" tabIndex="-1" href="http://maps.apple.com/?q=39.660609,-105.352111" target="_blank"><img class="img-two" src="_images/map.png" alt="Get Directions"></a>
 		</div>
 		<div id="hours-wrapper">
 			<span id="hours-background"></span>
 			<noscript><img src="_images/hours.png" alt="Hours"></noscript>
 		</div><!-- #hours-wrapper -->
+		<div class="info-bug-nowrap">
+			<a class="loc" href="https://www.facebook.com/evergreenbrewery/" target="_blank"><img class="info-bug-fb" src="_images/info-bug-facebook.png" alt="Facebook"></a><a class="loc" href="https://www.instagram.com/evergreenbrewery/" target="_blank"><img class="info-bug-i" src="_images/info-bug-instagram.png" alt="Instagram"></a><a class="loc" href="https://evergreenbrewery.menufy.com/#/" target="_blank"><img class="info-bug-oo" src="_images/info-bug-order-online.png" alt="Instagram"></a>
+		</div>
 	</div><!-- #info-bug -->
 
-	<div id="social-homepage">
-		<a href="https://www.facebook.com/evergreenbrewery/" target="_blank" onclick="closeNav()"><img src="_images/facebook-bw.png" alt="Facebook"></a>
-		<a href="https://www.instagram.com/evergreenbrewery/" target="_blank" onclick="closeNav()"><img src="_images/instagram-bw.png" alt="Instagram"></a>
-	</div>
-	<!-- 
-	<img itemprop="logo" id="logo" class="logo" src="_images/evergreen-brewery-bw-logo.png" /> 
-	-->
 </section>
